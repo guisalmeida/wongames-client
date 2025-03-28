0b3c1b72c03b625bb71c3afd2e149ee8b0230050
guisalmeida <guisalmeida.dev@gmail.com>
2025-03-28 00:06:59 -0300

    feat: implement mobile menu toggle functionality with tests

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,27 +1,37 @@
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2';
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart';
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search';
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
 
 import Logo from '../Logo';
 import * as S from './styles';
+import { useState } from 'react';
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon />
-    </S.IconWrapper>
-    <S.LogoWrapper>
-      <Logo $hideText />
-    </S.LogoWrapper>
-    <S.MenuGroup>
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <S.Wrapper>
       <S.IconWrapper>
-        <SearchIcon />
+        <MenuIcon aria-label="Open Menu" onClick={() => setIsOpen(true)} />
       </S.IconWrapper>
-      <S.IconWrapper>
-        <ShoppingCartIcon />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-);
+      <S.LogoWrapper>
+        <Logo $hideText />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} $isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  );
+};
 
 export default Menu;
