2202e865769649882ebe08a3a95502fd4e4df209
guisalmeida <guisalmeida.dev@gmail.com>
2025-03-29 00:00:39 -0300

    feat: enhance Menu component with user authentication display and responsive design

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -10,4 +10,14 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    userName: ''
+  },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+};
