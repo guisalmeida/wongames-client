67f859f0756c402a5b589d2c56d4543f7e7d76c4
guisalmeida <guisalmeida.dev@gmail.com>
2025-04-20 00:54:31 +0100

    test: enhance Footer component tests with additional assertions and snapshot validation

--- src/components/Footer/test.tsx
+++ src/components/Footer/test.tsx
@@ -1,11 +1,42 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 
 import Footer from '.';
+import { renderWithTheme } from '../../utils/tests/helpers';
 
 describe('<Footer />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Footer />)
-    expect(screen.getByRole('heading', { name: /Footer/i })).toBeInTheDocument()
-    expect(container.firstChild).toMatchSnapshot()
-  })
+  let container: HTMLElement;
+
+  beforeEach(() => {
+    container = renderWithTheme(<Footer />).container;
+  });
+
+  it('should match the snapshoot', () => {
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render 4 column topics', () => {
+    expect(screen.getAllByRole('heading')).toHaveLength(4);
+  });
+
+  it('should have contact us heading', () => {
+    expect(
+      screen.getByRole('heading', { name: /contact us/i })
+    ).toBeInTheDocument();
+  });
+
+  it('should have follow us heading', () => {
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument();
+  });
+
+  it('should have links heading', () => {
+    expect(screen.getByRole('heading', { name: /links/i })).toBeInTheDocument();
+  });
+
+  it('should have location heading', () => {
+    expect(
+      screen.getByRole('heading', { name: /location/i })
+    ).toBeInTheDocument();
+  });
 });
