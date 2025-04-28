0236d5fb99e785082ce6145cc22bd47ddde91af8
guisalmeida <guisalmeida.dev@gmail.com>
2025-04-28 23:32:43 +0100

    feat: enhance Highlight component with floatImage and alignment props

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -5,18 +5,23 @@ export type HighlightProps = {
   title: string;
   subtitle: string;
   backgroundImage: string;
+  floatImage?: string;
   buttonLabel: string;
   buttonLink: string;
+  alignment?: 'right' | 'left';
 };
 
 const Highlight = ({
   title,
   subtitle,
   backgroundImage,
+  floatImage,
   buttonLabel,
-  buttonLink
+  buttonLink,
+  alignment = 'right'
 }: HighlightProps) => (
-  <S.Wrapper backgroundImage={backgroundImage}>
+  <S.Wrapper alignment={alignment} backgroundImage={backgroundImage}>
+    {!!floatImage && <S.FloatImage src={floatImage} alt={title} />}
     <S.Content>
       <S.Title>{title}</S.Title>
       <S.Subtitle>{subtitle}</S.Subtitle>
