35ea89b9d80179f9f08a7f6d5014fccf68f5ee85
guisalmeida <guisalmeida.dev@gmail.com>
2025-04-30 23:41:50 +0100

    test: update Slider component tests to include item rendering and snapshot

--- src/components/Slider/test.tsx
+++ src/components/Slider/test.tsx
@@ -4,8 +4,21 @@ import Slider from '.';
 
 describe('<Slider />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Slider />)
-    expect(screen.getByRole('heading', { name: /Slider/i })).toBeInTheDocument()
-    expect(container.firstChild).toMatchSnapshot()
-  })
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>item 1</p>
+        <p>item 2</p>
+      </Slider>
+    );
+
+    expect(
+      screen.getByText(/item 1/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide');
+
+    expect(
+      screen.getByText(/item 2/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide');
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
