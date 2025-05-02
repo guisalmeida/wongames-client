0175e8e05d4bfdf932a2f45fcbd6fcff1760a1c7
guisalmeida <guisalmeida.dev@gmail.com>
2025-05-02 13:45:21 +0100

    feat: add GameCardSlider component with styles, stories, and tests

---
+++ src/components/GameCardSlider/stories.tsx
@@ -0,0 +1,71 @@
+import { Meta, StoryObj } from '@storybook/react';
+import GameCardSlider from '.';
+
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  }
+];
+
+const meta = {
+  title: 'GameCardSlider',
+  component: GameCardSlider,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} satisfies Meta<typeof GameCardSlider>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: { items },
+  render: (args) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+      <GameCardSlider {...args} />
+    </div>
+  )
+};
